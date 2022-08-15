4e584027094da086937fe8e268ef3a8edfc3f8ec
Karolis-Kvatavicius <karolis.kvatavicius@gmail.com>
2022-08-15 09:43:04 +0300

    Updated Resume

---
+++ js/jbox.js
@@ -0,0 +1,9 @@
+
+// document.cookie = your_old_cookie + "; Secure; SameSite=None; path=/";
+
+new jBox('Tooltip', {
+    position: { x: 'center', y: 'bottom' },
+    delayClose: 500,
+    theme: 'TooltipDark',
+    attach: '.tooltips',
+});
